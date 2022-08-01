29ea89a9e198650a4783eb4256e90a7b3b107453
Wayd26 <wayedoun@gmail.com>
2022-08-01 11:23:46 +0100

    update utils file and Projects.js for deployment

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,5 +1,5 @@
 export const validateEmail = (mail) => {
-    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail)) {
+    if (/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(mail)) {
         return (true)
     }
     return (false)
