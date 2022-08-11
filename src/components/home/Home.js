8c45aa9f6d00e7dd133402bd5dbbb1b7682fc419
Wayd26 <wayedoun@gmail.com>
2022-08-11 12:35:11 +0100

    update profile pic on home screen and favicon

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -8,7 +8,7 @@ import Typewriter from 'typewriter-effect'
 import ArrowDropDownCircleIcon from '@material-ui/icons/ArrowDropDownCircle'
 import Navbar from '../navbar/Navbar'
 import config from '../../config'
-import profile from '../../images/wayd.png'
+import profile from '../../images/wayd.jpeg'
 import linkedin from '../../images/social/linkedin.png'
 
 const Home = () => {
